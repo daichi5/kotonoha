1bd09baa3967c065088ac2e7a86514f567d45eff
daichi <sw41dc@gmail.com>
2019-03-06 22:27:00 +0900

    test jenkins build

--- spec/views/static_pages/home.html.erb_spec.rb
+++ spec/views/static_pages/home.html.erb_spec.rb
@@ -3,6 +3,6 @@ require 'rails_helper'
 RSpec.describe "static_pages/home.html.erb", type: :view do
   it 'render home page' do
     render
-    expect(rendered).to include("hogehoge")
+    expect(rendered).to include("kotonoha")
   end
 end
