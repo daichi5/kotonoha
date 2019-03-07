9cf4b761cb421c1731f8782c1db74f9c593c66d9
daichi <sw41dc@gmail.com>
2019-03-07 15:17:20 +0900

    modify spec

--- spec/views/static_pages/about.html.erb_spec.rb
+++ spec/views/static_pages/about.html.erb_spec.rb
@@ -1,10 +1,4 @@
 require 'rails_helper'
 
 RSpec.describe "static_pages/about.html.erb", type: :view do
-  describe do
-    it 'render home page' do
-      render
-      expect(rendered).to include("kotonoha")
-    end
-   end
 end
--- spec/views/static_pages/home.html.erb_spec.rb
+++ spec/views/static_pages/home.html.erb_spec.rb
@@ -1,8 +1,4 @@
 require 'rails_helper'
 
 RSpec.describe "static_pages/home.html.erb", type: :view do
-  it 'render home page' do
-    render
-    expect(rendered).to include("kotonoha")
-  end
 end
