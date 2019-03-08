a6fa6a40e5de9e91267c321fc878023623bce406
daichi <sw41dc@gmail.com>
2019-03-08 14:51:30 +0900

    modify signup function

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   get root 'static_pages#home'
-  get 'about' => 'static_pages#about'
-  get 'help'  => 'static_pages#help'
+  get '/about' => 'static_pages#about'
+  get '/help'  => 'static_pages#help'
+
+  get '/signup' => 'users#new'
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2019_03_08_041836) do
+
+  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
