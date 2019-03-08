247dd333f32b3daeb8869c7d4b8989d90bd74578
daichi <sw41dc@gmail.com>
2019-03-08 16:48:00 +0900

    update user function

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,13 @@
 class UsersController < ApplicationController
+  
+  def index
+    @users = User.all
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+  
   def new
     @user = User.new();
   end
@@ -7,9 +16,9 @@ class UsersController < ApplicationController
     @user = User.new(user_params);
 
     if @user.save
-
-    else
       redirect_to root_path
+    else
+      render 'new'
     end
   end
 
