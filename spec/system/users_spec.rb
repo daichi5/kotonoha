0074c4f7d9aec39182b4e8965e7f443096ac73a1
daichi <sw41dc@gmail.com>
2019-04-14 21:59:00 +0900

    :rotating_light: Add 'Signup failure' Test
    RSpecテストの追加
    内容：名前無しで登録をした時にエラーを表示する

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -4,15 +4,25 @@ describe 'User', type: :system do
   describe 'user signup' do
     before do
       visit signup_path
-      fill_in 'Name', with: 'test_user'
-      fill_in 'Email', with: 'test_user@example.com'
-      fill_in 'Password', with: 'password'
+      fill_in User.human_attribute_name(:name), with: 'test_user'
+      fill_in User.human_attribute_name(:email), with: 'test_user@example.com'
+      fill_in User.human_attribute_name(:password), with: 'password'
       fill_in 'Confirmation', with: 'password'
-      click_button '登録'
     end
 
-    it 'success' do
+    it 'should success' do
+      click_button '登録'
+
       expect(page).to have_content '登録を受け付けました'
+    end
+
+    it 'should failure with invalid name' do
+      fill_in User.human_attribute_name(:name), with: ''
+      click_button '登録'
+
+      expect(page).not_to have_content '登録を受け付けました'
+      expect(page).to have_css('.alert')
+
     end
   end
 end
