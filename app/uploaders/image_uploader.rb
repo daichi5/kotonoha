800613dd84cbc03ae71b63b47b90c192c015356f
daichi <sw41dc@gmail.com>
2019-04-15 16:59:20 +0900

    :sparkles: アップロード画像をリサイズ（上限256*256)

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,8 +1,9 @@
 class ImageUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
-  # include CarrierWave::RMagick
+  include CarrierWave::RMagick
   # include CarrierWave::MiniMagick
 
+  process :resize_to_limit => [256, 256]
   # Choose what kind of storage to use for this uploader:
   storage :file
   # storage :fog
