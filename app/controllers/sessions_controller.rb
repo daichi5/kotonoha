d5220d1d200ac5b66a99882a04d2938183091e87
daichi <sw41dc@gmail.com>
2019-04-15 17:48:46 +0900

    :bug: flashメッセージの修正
    flashをflash.nowに修正

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,14 +10,14 @@ class SessionsController < ApplicationController
       if (session_params[:remember_me] == '1') 
         remember(user)
       end 
-      flash[:success] = "success!!#{cookies.signed[:user_id].inspect}"
+      flash[:success] = "success!!"
       session[:user_id] = user.id
 
       #friendly forwarding
       redirect_to ( session[:forwarding_url] || user ) 
       session.delete(:forwarding_url)
     else
-      flash[:danger] = 'Emailまたはパスワードが間違っています'
+      flash.now[:danger] = 'Emailまたはパスワードが間違っています'
       render 'new'
     end
   end
