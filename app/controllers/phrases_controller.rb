3fa2b31b87871b2298f01ef3acb7f797b854135d
daichi <sw41dc@gmail.com>
2019-04-16 16:00:07 +0900

    :sparkles: phrase投稿機能を追加

--- app/controllers/phrases_controller.rb
+++ app/controllers/phrases_controller.rb
@@ -3,11 +3,29 @@ class PhrasesController < ApplicationController
   end
 
   def show
+    @phrase = Phrase.find(params[:id])
   end
 
   def new
+    @phrase = current_user.phrases.new
+  end
+
+  def create
+    @phrase = current_user.phrases.new(phrase_params)
+    if @phrase.save
+      flash[:success] = '投稿送信完了'
+      redirect_to current_user
+    else
+      render :new
+    end
+
   end
 
   def edit
+  end
+
+  private
+  def phrase_params
+    params.require(:phrase).permit(:title, :content)
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,6 +9,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @phrases = @user.phrases.all.order("updated_at DESC")
   end
   
   def new
