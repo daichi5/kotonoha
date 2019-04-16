2d27c6df65a84dd18954fec944662d9d60fdfe8f
daichi <sw41dc@gmail.com>
2019-04-16 11:57:43 +0900

    :rotating_light: user_model_validationテストを追加
    userモデルのvalidatesに対してテスト

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,4 +1,36 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  it "is valid with name, email, and password" do
+    user = User.new(
+      name: "test",
+      email: "test@example.com",
+      password: "password",
+      password_confirmation: "password"
+    )
+    expect(user).to be_valid
+  end
+
+  it "is invalid without a name" do
+    user = User.new(name: "")
+    user.valid?
+    expect(user.errors[:name]).to include("を入力してください")
+  end
+
+  it "is invalid without an email address" do
+    User.create(
+      name: "test",
+      email: "test@example.com",
+      password: "password",
+      password_confirmation: "password"
+    )
+    user = User.new(
+      email: "test@example.com",
+    )
+    user.valid?
+    expect(user.errors[:email]).to include("はすでに存在します")
+  end
+  
+  it "is invalid with a duplicate email address"
+  it "returns a user's full name as a string"
 end
--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -17,14 +17,14 @@ describe 'User', type: :system do
         user_count = User.all.count
       }.to change { user_count }
 
-      expect(page).to have_content '登録を受け付けました'
+      expect(page).to have_content '登録が完了'
     end
 
     it 'should failure with invalid name' do
       fill_in User.human_attribute_name(:name), with: ''
       click_button '登録'
 
-      expect(page).not_to have_content '登録を受け付けました'
+      expect(page).not_to have_content '登録が完了'
       expect(page).to have_css('.alert')
     end
 
