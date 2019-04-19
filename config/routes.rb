f583b514424fe6c25b5bf2ff60a194d782992b9c
daichi <sw41dc@gmail.com>
2019-04-19 14:11:10 +0900

    :recycle: Add likes_routing

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,13 @@ Rails.application.routes.draw do
 
   get '/signup' => 'users#new'
   post '/signup' => 'users#create'
-  resources :users, except: [:new, :create]
+  resources :users, except: [:new, :create] do
+    member do
+      resource :likes, only: :show
+    end
+  end
+  resources :likes, only: [:create, :destroy]
+
 
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
