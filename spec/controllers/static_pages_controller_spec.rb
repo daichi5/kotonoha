61b7b3674d499799103e54c176a23bb5721cba22
daichi <sw41dc@gmail.com>
2019-04-20 17:24:06 +0900

    :rotating_light: Add Rspec /user_controller

--- spec/controllers/static_pages_controller_spec.rb
+++ spec/controllers/static_pages_controller_spec.rb
@@ -1,4 +1,39 @@
 require 'rails_helper'
 
 RSpec.describe StaticPagesController, type: :controller do
+  describe "#home" do
+    it "reponds successfully" do
+      get :home
+      expect(response).to be_successful
+    end
+
+    it "returns a 200 response" do
+      get :home
+      expect(response).to have_http_status "200"
+    end
+  end
+
+  describe "#about" do
+    it "reponds successfully" do
+      get :about
+      expect(response).to be_successful
+    end
+
+    it "returns a 200 response" do
+      get :about
+      expect(response).to have_http_status "200"
+    end
+  end
+
+  describe "#help" do
+    it "reponds successfully" do
+      get :help
+      expect(response).to be_successful
+    end
+
+    it "returns a 200 response" do
+      get :help
+      expect(response).to have_http_status "200"
+    end
+  end
 end
