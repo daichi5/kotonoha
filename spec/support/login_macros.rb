9b7c603c1a7249439ccb9a35018c5ab621aa359f
daichi <sw41dc@gmail.com>
2019-04-24 21:38:38 +0900

    :rotating_light: Add SystemSpec user_create_a_phrase
    ユーザー投稿のSystemSpecを追加

--- spec/support/login_macros.rb
+++ spec/support/login_macros.rb
@@ -1,5 +1,13 @@
 module LoginMacros
+  #単体テスト用ログインメソッド
   def login(user)
     session[:user_id] = user.id
+  end
+  #システムテスト用ログインメソッド
+  def login_by_capybara(user)
+    visit login_path
+    fill_in 'メールアドレス', with: user.email
+    fill_in 'パスワード', with: user.password
+    click_button 'ログイン'
   end
 end
