79ba147d47936f8ce012c4fccabcce9025fdc680
daichi <sw41dc@gmail.com>
2019-04-30 23:31:30 +0900

    :rotating_light: コメント投稿の統合テスト追加

---
+++ spec/system/comments_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe 'Comments', type: :system, js: true do
+  it 'visitor creates a comment' do
+    phrase = FactoryBot.create(:phrase)
+    
+    expect {
+      visit "/phrases/#{phrase.id}"
+      fill_in "名前", with: "sample name"
+      fill_in "コメント", with: "sample comment"
+      click_button "投稿"
+      expect(page).to have_content("sample name")
+      expect(page).to have_content("sample comment")
+    }.to change(phrase.comments, :count).by(1)
+
+
+  end
+end
