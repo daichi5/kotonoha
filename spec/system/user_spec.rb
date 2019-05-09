9d9e78f684286b20589886f37c7d823e56dfb45c
sawa <sw41dc@gmail.com>
2019-05-09 16:54:44 +0900

    [TEST] ユーザー削除機能の統合テストを実装

--- spec/system/user_spec.rb
+++ spec/system/user_spec.rb
@@ -1,6 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe 'Users', type: :system do
+  before do
+    @user = FactoryBot.create(:user, name: "testuser", email: "test@example.com")
+    login_as(@user)
+  end
+
   it 'user edits a profile'
-  it 'user deletes an account' 
+
+
+  it 'user deletes an account', js: true do
+    visit "/users/#{user.id}"
+    expect{
+      click_button '削除'
+      click_button 'OK'
+    }.to change(User, :count).by(-1)
+
+    expect(response).to redirect_to('/');
+  end
 end
