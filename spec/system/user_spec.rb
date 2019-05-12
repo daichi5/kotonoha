fc043a5fd9d93fe6885c51c9ec4d993fe687030c
sawa <sw41dc@gmail.com>
2019-05-12 11:54:01 +0900

    [TEST] ユーザー編集機能の統合テスト追加

--- spec/system/user_spec.rb
+++ spec/system/user_spec.rb
@@ -1,10 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe 'Users', type: :system, js: true do
-  it 'user deletes an account' do
-    user = FactoryBot.create(:user, name: "testuser", email: "test@example.com")
-    login_as(user)
+  before do
+    @user = FactoryBot.create(:user, name: "testuser", email: "test@example.com")
+    login_as(@user)
+  end
 
+  it 'user deletes an account' do
     expect{
       click_link 'アカウント'
       click_link 'マイページ'
@@ -13,7 +15,24 @@ RSpec.describe 'Users', type: :system, js: true do
       expect(text).to eq('アカウントを削除します。よろしいですか？')
       page.accept_confirm()
       visit '/'
-      expect(User.find_by(id: user.id)).to be_falsy
+      expect(User.find_by(id: @user.id)).to be_falsy
     }.to change(User, :count).by(-1)
   end
+
+  it 'user edits a profile' do
+    click_link 'アカウント'
+    click_link 'マイページ'
+    click_link '編集'
+    fill_in '名前', with: 'updated name'
+    fill_in 'メールアドレス', with: 'updated@example.com'
+    fill_in '自己紹介', with: 'updated description'
+    click_button '変更を保存'
+    @user.reload
+
+    expect(@user.name).to eq('updated name')
+    expect(@user.email).to eq('updated@example.com')
+    expect(@user.description).to eq('updated description')
+
+  end
 end
+
