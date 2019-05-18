521a61e4557143713766ef0ce4e4373d8a3b5df8
sawa <sw41dc@gmail.com>
2019-05-18 22:07:32 +0900

    :sparkles: Phraseモデルにurlカラムを追加
    投稿の引用元URL記載用にurlカラムを追加
    ・マイグレーションファイル作成
    ・投稿フォームにフィールド追加
    ・モデルにURLバリデーション追加
    ・ストロングパラメーターにurlを追加
    ・日本語翻訳ファイルにurlカラムを追加

--- app/models/phrase.rb
+++ app/models/phrase.rb
@@ -4,4 +4,5 @@ class Phrase < ApplicationRecord
   has_many :comments, dependent: :destroy
   validates :title, presence: true
   validates :content, presence: true
+  validates :url, format: /\A#{URI::regexp(%w(http https))}\z/, allow_blank: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_29_050202) do
+ActiveRecord::Schema.define(version: 2019_05_18_070959) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,6 +42,7 @@ ActiveRecord::Schema.define(version: 2019_04_29_050202) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "url"
     t.index ["user_id", "created_at"], name: "index_phrases_on_user_id_and_created_at"
     t.index ["user_id"], name: "index_phrases_on_user_id"
   end
