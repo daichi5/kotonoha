fd244f7e09c7e6e23102fc6bceb919b69638ff3d
sawa <sw41dc@gmail.com>
2019-05-18 15:32:53 +0900

    :art: フッターメニュー修正
    フッタメニューのデザイン修正
    フッタメニューを１つ削除
    ヘッダーボックスの高さ変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get root 'static_pages#home'
   get '/about' => 'static_pages#about'
-  get '/help'  => 'static_pages#help'
+  get '/contact'  => 'static_pages#contact'
 
   get '/signup' => 'users#new'
   post '/signup' => 'users#create'
