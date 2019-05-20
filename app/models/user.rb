18e3024132356695dda22adb3b6fc632a0bbed70
sawa <sw41dc@gmail.com>
2019-05-20 19:38:25 +0900

    :recycle: Modelリファクタリング
    modelファイルをリファクタリング
    不要なコメントを削除
    冗長な構文を短縮

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,20 +4,17 @@ class User < ApplicationRecord
   has_many :phrases, dependent: :destroy
   has_many :likes, dependent: :destroy
   has_many :liked_phrases, through: :likes, source: :phrase
-  has_secure_password
-  mount_uploader :image, ImageUploader
-
   validates :name, presence: true, length: { maximum: 20 }
   validates :email, presence: true, uniqueness: { case_sensitive: false }, format: { with: VALID_EMAIL_REGEX }
   validates :password, presence: true, Length: { minimum: 6 }, on: :create
+  has_secure_password
+  mount_uploader :image, ImageUploader
 
-  #ハッシュ化
   def User.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
     BCrypt::Password.create(string, cost: cost)
   end
 
-  #トークン生成用, ランダムな文字列を返す
   def User.new_token
     SecureRandom.urlsafe_base64
   end
@@ -27,7 +24,6 @@ class User < ApplicationRecord
     update_attribute(:remember_digest, User.digest(remember_token))
   end
 
-  #rememberトークンの照合
   def authenticated?(remember_token)
     BCrypt::Password.new(remember_digest).is_password?(remember_token)
   end
