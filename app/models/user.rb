f5b8063817a4057904eea7f38e3c3d152522cde1
sawa <sw41dc@gmail.com>
2019-05-21 17:18:14 +0900

    :rotating_light: Phraseモデルに対して単体テストを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
   has_many :liked_phrases, through: :likes, source: :phrase
   validates :name, presence: true, length: { maximum: 20 }
   validates :email, presence: true, uniqueness: { case_sensitive: false }, format: { with: VALID_EMAIL_REGEX }
-  validates :password, presence: true, Length: { minimum: 6 }, on: :create
+  validates :password, presence: true, length: { minimum: 6 }, on: :create
   validates :description, length: { maximum: 120 }
   has_secure_password
   mount_uploader :image, ImageUploader
--- spec/models/phrase_spec.rb
+++ spec/models/phrase_spec.rb
@@ -3,13 +3,58 @@ require 'rails_helper'
 RSpec.describe Phrase, type: :model do
   let(:phrase) { FactoryBot.build(:phrase) }
 
-  it "is valid with title, description, url" do
+  it "is valid with title, content, url" do
     expect(phrase).to be_valid
   end
 
   it "is invalid without a title" do
     phrase.title = nil
-    expect(phrase).to_not be_valid
+    phrase.valid?
+    expect(phrase.errors["title"]).to include('を入力してください')
+  end
+
+  it "is valid with a title of 150characters" do
+    phrase.title = "a" * 150
+    expect(phrase).to be_valid
+  end
+
+  it "is invalid with a title over 150 characters" do
+    phrase.title = "a" * 151
+    phrase.valid?
+    expect(phrase.errors["title"]).to include('は150文字以内で入力してください')
+  end
+
+  it "is valid with a content of 200characters" do
+    phrase.content = "a" * 200
+    expect(phrase).to be_valid
+  end
+
+  it "is invalid with a content over 200 characters" do
+    phrase.content = "a" * 201
+    phrase.valid?
+    expect(phrase.errors["content"]).to include('は200文字以内で入力してください')
+  end
+
+  it "is valid with a author of 50characters" do
+    phrase.author = "a" * 50
+    expect(phrase).to be_valid
+  end
+
+  it "is invalid with a author over 50 characters" do
+    phrase.author = "a" * 51
+    phrase.valid?
+    expect(phrase.errors["author"]).to include('は50文字以内で入力してください')
+  end
+
+  it "is valid with a quoted of 800characters" do
+    phrase.quoted = "a" * 800
+    expect(phrase).to be_valid
+  end
+
+  it "is invalid with a quoted over 800 characters" do
+    phrase.quoted = "a" * 801
+    phrase.valid?
+    expect(phrase.errors["quoted"]).to include('は800文字以内で入力してください')
   end
 
   it "is invalid without a user" do
