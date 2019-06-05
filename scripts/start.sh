23c2e9bf01c1b437f5f1f052df58b7c0abe92e5d
sawa <sw41dc@gmail.com>
2019-06-05 09:46:34 +0900

    :construction: ステージング環境への自動デプロイを停止
    ・ステージング環境では本番環境のRDSを共有している
    ・ステージング環境への自動デプロイ振り分けロジックが不完全
    
    以上の理由からステージング環境への自動デプロイを停止
    ECSの導入時にステージング環境を再度構成、稼働し直す予定

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 
-git fetch origin staging
-git merge origin/staging
-
 export RAILS_MASTER_KEY=$(aws ssm get-parameters --region ap-northeast-1  --name 'RAILS_MASTER_KEY' | jq -r '.Parameters[].Value')
 export KOTONOHA_DATABASE_PASSWORD=$(aws ssm get-parameters --region ap-northeast-1 --name 'KOTONOHA_DATABASE_PASSWORD' | jq -r '.Parameters[].Value')
 
