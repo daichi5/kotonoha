e2af6c7ddcba22c5762048d6eb01ac725f663ed0
sawa <sw41dc@gmail.com>
2019-06-08 02:13:00 +0900

    :sparkles: 本番環境のコンテナ管理をECSへ全面移行
    EC2上でdockerを起動させて運用していたが
    コンテナの状態管理とスケールイン・アウトを考慮し（特に状態管理）
    ECSでの管理に全面移行。
    CircleCIを通じて自動的にイメージビルド、ECRへプッシュ→タスク定義の更新が実行される。
    
    今後のコンテナは自動起動され、停止したとしても再起動される（感動

--- scripts/start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-export RAILS_MASTER_KEY=$(aws ssm get-parameters --region ap-northeast-1  --name 'RAILS_MASTER_KEY' | jq -r '.Parameters[].Value')
-export KOTONOHA_DATABASE_PASSWORD=$(aws ssm get-parameters --region ap-northeast-1 --name 'KOTONOHA_DATABASE_PASSWORD' | jq -r '.Parameters[].Value')
-
-docker-compose --file /var/www/rails/kotonoha/docker-compose.ec2.yml down
-docker-compose --file /var/www/rails/kotonoha/docker-compose.ec2.yml up -d
