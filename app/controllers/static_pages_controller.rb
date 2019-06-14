0d3eb9bc4161df2de0447b239dfa1305538b02b4
sawa <sw41dc@gmail.com>
2019-06-14 17:36:59 +0900

    :bug: マイページとトップページの表示バグを修正
    ・マイページのレスポンシブデザインが崩れていた
    ・「今よく見られている記事」のタグといいね數が表示されていない
    上記のバグを修正

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -4,7 +4,7 @@ class StaticPagesController < ApplicationController
   def home
     date = Date.today.strftime(format = '%Y%m%d')
     popular_id = Redis.current.zrevrangebyscore(date, '+inf', '-inf').first 
-    @phrase = Phrase.find_by(id: popular_id)
+    @phrase = Phrase.set_buttons.find_by(id: popular_id)
     @phrases = Phrase.set_buttons.order(created_at: "DESC").page(params[:page])
   end
 
