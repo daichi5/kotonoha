c808b6096d46b913039ff07609e12aa59ea1e489
sawa <sw41dc@gmail.com>
2019-06-14 18:32:48 +0900

    :refactor: PhraseModelとControllerのリファクタリング
    Model寄りの処理とControllerの分け方が
    曖昧だったためMVCを整理

--- app/controllers/phrases_controller.rb
+++ app/controllers/phrases_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class PhrasesController < ApplicationController
+  helper_method :modified_url
   before_action :authenticate_user!, only: %i[new create edit]
 
   def index
@@ -14,7 +15,7 @@ class PhrasesController < ApplicationController
   def show
     @phrase = Phrase.set_buttons.find(params[:id])
     @comment = @phrase.comments.build
-    @pv = access_count(@phrase)
+    @pv = @phrase.access_count(request.remote_ip)
   end
 
   def new
@@ -55,35 +56,18 @@ class PhrasesController < ApplicationController
   private
 
   def phrase_params
-    params[:phrase][:url_title] = save_url_title(params[:phrase][:quoted])
+    params[:phrase][:url_title] = @phrase.save_url_title(params[:phrase][:quoted])
     params.require(:phrase).permit(:title, :content, :author, :quoted, :url_title, :tag_list)
   end
 
-  def access_count(phrase)
-    redis = Redis.current
-    key = request.remote_ip.to_s + ":" + phrase.id.to_s
-    date = Date.today.strftime(format = '%Y%m%d')
-    unless redis.exists(key)
-      ttl = 60 * 60 * 3
-      redis.zincrby(date, 1, phrase.id)
-      redis.set(key, true)
-      redis.expire(key, ttl)
-    end
-    redis.zscore(date, phrase.id).to_i
-  end
+  #view helper
 
-  def save_url_title(url)
-    if @phrase&.quoted != url
-      scraping_title(url)
-    elsif @phrase
-      @phrase.url_title
+  def modified_url(phrase)
+    url = phrase.quoted
+    if phrase.url_title.present? && url.length > 30
+      url[0..29] + "..."
+    else
+      url
     end
-  end
-
-  def scraping_title(url)
-    title = Nokogiri::HTML.parse(open(url)).title
-    title.length > 30 ? (title[0..29] + '...') : title
-  rescue StandardError
-    nil
   end
 end
